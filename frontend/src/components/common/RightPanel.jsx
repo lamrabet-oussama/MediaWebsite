958dd8921b07e80e8c0530072e6af90dc6c2f31d
lamrabet-oussama <lamrabetoussama184@gmail.com>
2024-11-09 11:44:18 +0100

    Add suggested Users

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,13 +1,28 @@
 import { Link } from "react-router-dom";
 import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
 import { USERS_FOR_RIGHT_PANEL } from "../../utils/db/dummy";
-
+import { useQuery } from "@tanstack/react-query";
 const RightPanel = () => {
-  const isLoading = true;
-
+  const { data: suggestedUsers, isLoading } = useQuery({
+    queryKey: ["suggestedUsers"],
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/users/suggested");
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.message || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+  });
+  if (suggestedUsers.suggestedUsers?.length === 0)
+    return <div className="md:w-64 w-0"></div>;
   return (
     <div className="hidden fixed w-[23rem] right-0 top-0 lg:block my-4 mx-2">
-      <div className="border border-[#FAB400] rounded-md text-[#FAB400] p-4 rounded-md sticky top-2">
+      <div className="border border-[#FAB400] rounded-md text-[#FAB400] p-4  sticky top-2">
         <p className="font-bold">Who to follow</p>
         <div className="flex flex-col gap-4">
           {/* item */}
@@ -20,7 +35,7 @@ const RightPanel = () => {
             </>
           )}
           {!isLoading &&
-            USERS_FOR_RIGHT_PANEL?.map((user) => (
+            suggestedUsers.suggestedUsers?.map((user) => (
               <Link
                 to={`/profile/${user.username}`}
                 className="flex items-center justify-between gap-4 border-b-2 border-white p-2"
